3b33a8575c9c27405f7246b5f3b8724a8d040169
Vagner Ferreira de Melo <165937363+Vagnermelo23@users.noreply.github.com>
2024-04-16 15:13:45 -0300

    xd

--- microblog.py
+++ microblog.py
@@ -1,6 +1,5 @@
 from app import app
 import os 
-
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.getenv("PORT", '5000'))
+    app.run(host = '0.0.0.0' , port = port )
