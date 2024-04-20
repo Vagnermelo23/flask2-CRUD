1d16992dc087b889001e74c4482f9719941e8bc1
Vagner Ferreira de Melo <165937363+Vagnermelo23@users.noreply.github.com>
2024-04-20 19:07:23 -0300

    resquest

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from flask import request
 
 
 @app.route('/')
@@ -11,3 +12,12 @@ def index_default():
 def index(nome):
     return render_template('index.html', nome=nome)
 
+@app.route ('/login')
+def login ():
+    return render_template('login.html')
+
+@app.route ('/autenticar', methods=['GET'])
+def autenticar ():
+    usuario = request.args.get('usuario')
+    senha = request.args.get('senha')
+    return f'usuario: {usuario} senha:{senha}'         
