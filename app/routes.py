e769cc4d321cd58a85a771e3b9daa90b61452054
Vagner Ferreira de Melo <165937363+Vagnermelo23@users.noreply.github.com>
2024-04-22 16:27:06 -0300

    implementando metodo POST  registrar

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template, redirect, url_for
 from flask import request
 
+lista = []
 
 @app.route('/')
 def index_default():
@@ -21,3 +22,21 @@ def autenticar ():
     usuario = request.args.get('usuario')
     senha = request.args.get('senha')
     return redirect(url_for('index', nome=usuario))
+
+
+
+@app.route('/registrar')
+def registrar():
+    return render_template('registrar.html')
+
+    
+@app.route('/autenticar1', methods=['POST'])
+def autenticar1():
+    usuario1 = request.form.get('usuario1')
+    senha1 = request.form.get('senha1')
+    return redirect(url_for('registrado', usuario1=usuario1))
+
+@app.route('/registrado')
+def registrado ():
+    usuario1 = request.args.get('usuario1')
+    return render_template('registrado.html', usuario1=usuario1)
